2d212cc22a9b2dcb860954738af9eafd0f5d4b7d
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-09-15 11:14:27 +0200

    Improved input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace ChessBoard
@@ -7,27 +8,20 @@ namespace ChessBoard
 
     internal class Program
     {
+
         static void Main(string[] args)
         {
             //unicode to show the squares, and setting a unicode standard output
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
 
-            //Default squares and chesspiece if user dont choose
-            string defaultPiece = "♟️";
-            string defaultBlack = "◼︎";
-            string defaultWhite = "◻︎";
-            string placementDefualt = "A1";
-
-            //Variables to hold choices
-            string black;
-            string white;
-            string piece;
-            string sizeInput;
-            string placement;
+            //Variables with default choices
+            string black = "◼︎";
+            string white = "◻︎";
+            string piece = "♟️";
+            string placement = "A1";
             int boardSize = 0;
-
-
+            string sizeInput;
 
             //loop until valid input
             do
@@ -36,68 +30,29 @@ namespace ChessBoard
                 sizeInput = Console.ReadLine();
             } while (!Int32.TryParse(sizeInput, out boardSize) || boardSize > 26);
 
-
-
-            //choose black look. if null or empty choose default
+            //Take input for choices
             Console.WriteLine("Hur ska svarta rutor se ut? (lämna tomt för standard)");
-            string blackInput = Console.ReadLine();
-            if (blackInput == null || blackInput == "")
-            {
-                black = defaultBlack;
-            }
-            else
-            {
-                black = blackInput;
-            }
+            black = TakeInput(black);
 
-
-            //choose white look. if null or empty choose default
             Console.WriteLine("Hur ska vita rutor se ut? (lämna tomt för standard)");
-            string whiteInput = Console.ReadLine();
-            if (whiteInput == null || whiteInput == "")
-            {
-                white = defaultWhite;
-            }
-            else
-            {
-                white = whiteInput;
-            }
+            white = TakeInput(white);
 
-            //choose Chess piece. if null or empty choose default
             Console.WriteLine("Hur ska pjäsen se ut (lämna tomt för standard)");
-            string pieceInput = Console.ReadLine();
-            if (pieceInput == null || pieceInput == "")
-            {
-                piece = defaultPiece;
-            }
-            else
-            {
-                piece = pieceInput;
-            }
+            piece = TakeInput(piece);
 
-            //choose where to place piece. if null or empty choose default
             Console.WriteLine("Var ska pjäsen stå? (ex. E4)");
-            string placementInput = Console.ReadLine();
-            if (placementInput == null || placementInput == "")
-            {
-                placement = placementDefualt;
-            }
-            else
-            {
-                placement = placementInput;
-            }
+            placement = TakeInput(placement);
 
+         
             StringBuilder stringBuilder = new StringBuilder();
+
             //convert letter position in alphabet to int by removing 64 from ASCII position to get column
             //Remove first letter using stringbuilder and parse number to int to get row
             stringBuilder.Append(placement.ToUpper());
             int column = stringBuilder[0] - 64;
-            stringBuilder.Remove(0,1);
+            stringBuilder.Remove(0, 1);
             int row = Int32.Parse(stringBuilder.ToString());
-            
 
-            
-            
 
             //Loop to create board and place chess piece using stringbuilder
             //Start "i" loop at the top and count down to make bottom row 1 for piece placement 
@@ -140,5 +95,25 @@ namespace ChessBoard
 
 
         }
+
+
+        //Take input. If empty/null return default value;
+        static string TakeInput(string X)
+        {
+            string input = Console.ReadLine();
+
+            if (input == null || input == "")
+            {
+                return X;
+            }
+            else
+            {
+                return input;
+            }
+        }
+
     }
+
+
+
 }
